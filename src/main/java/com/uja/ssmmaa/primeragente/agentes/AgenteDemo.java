9234e63582391034ed877997a57401fd12e7f966
Daniel Carrasco de la Chica <daniel230401@gmail.com>
2023-02-17 10:01:54 +0100

    cambios hechos

--- src/main/java/com/uja/ssmmaa/primeragente/agentes/AgenteDemo.java
+++ src/main/java/com/uja/ssmmaa/primeragente/agentes/AgenteDemo.java
@@ -18,46 +18,57 @@ import jade.domain.FIPAException;
  * @author pedroj
  */
 public class AgenteDemo extends Agent {
+
     // Constantes
-    public static final long TIEMPO_CICLO = 10000; // 10 seg.
-    
+    public static final long TIEMPO_CICLO = 5000; // 10 seg.
+
     // Variables del agente
     private AgenteDemoJFrame myGui;
     private int ejecuciones;
-    
+    private int EJECUCIONES_MAXIMAS = 5;
+    //private AgenteMonitor agMonitor;
+
     /**
      * Se ejecuta cuando se inicia el agente
      */
     @Override
     protected void setup() {
         //Configuración del GUI y presentación
+        System.getProperty("java.classpath");
         myGui = new AgenteDemoJFrame(this);
         myGui.setVisible(true);
-        myGui.presentarSalida("Se inicia la ejecución de " + this.getName() + "\n");
-        
+        myGui.presentarSalida("Se inicializa la ejecución de " + this.getName() + "\n");
+
         //Incialización de variables
         ejecuciones = 0;
-        
+
+        //obtengo el argumento
+        Object[] args = getArguments();
+
+        if (args != null && args.length > 0) {
+            String argumento = (String) args[0];
+            EJECUCIONES_MAXIMAS = Integer.parseInt(argumento);
+        }
+        //inicializacion agente monitor
+        //agMonitor=new AgenteMonitor();
         //Registro de la Ontología
-        
         //Registro en Página Amarillas
         DFAgentDescription dfd = new DFAgentDescription();
         dfd.setName(getAID());
-	ServiceDescription sd = new ServiceDescription();
-	sd.setType("Tipo de Servicio");
-	sd.setName("Nombre del Servicio");
-	dfd.addServices(sd);
-	try {
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("Tipo de Servicioo");
+        sd.setName("Nombre del Servicioo");
+        dfd.addServices(sd);
+        try {
             DFService.register(this, dfd);
-	}
-	catch (FIPAException fe) {
+        } catch (FIPAException fe) {
             fe.printStackTrace();
-	}
-        
+        }
+
         // Se añaden las tareas principales
         addBehaviour(new TareaEjemplo(this, TIEMPO_CICLO));
     }
-    
+
     /**
      * Se ejecuta al finalizar el agente
      */
@@ -66,21 +77,19 @@ public class AgenteDemo extends Agent {
         //Desregistro de las Páginas Amarillas
         try {
             DFService.deregister(this);
-	}
-            catch (FIPAException fe) {
+        } catch (FIPAException fe) {
             fe.printStackTrace();
-	}
-        
+        }
+
         //Se liberan los recuros y se despide
         myGui.dispose();
         System.out.println("Finaliza la ejecución de " + this.getName());
     }
-    
+
     //Métodos del agente
-    
-    
     //Clases que representan las tareas del agente
     public class TareaEjemplo extends TickerBehaviour {
+
         //Tarea de ejemplo que se repite cada 10 segundos
         public TareaEjemplo(Agent a, long period) {
             super(a, period);
@@ -88,10 +97,36 @@ public class AgenteDemo extends Agent {
 
         @Override
         protected void onTick() {
-           ejecuciones++;
-           myGui.presentarSalida("\nEjecución número: " + ejecuciones);
+            ejecuciones++;
+            myGui.presentarSalida("\nEjecución número: " + ejecuciones + "\n");
+            System.out.println("\nEjecución número: " + ejecuciones + "\n");
+            if (ejecuciones == EJECUCIONES_MAXIMAS) {
+                myAgent.doDelete();
+                System.out.println("Finaliza la ejecución de " + this.myAgent.getName() + " por llegar a las " + ejecuciones + " ejecuciones");
+            } else {
+                if (ejecuciones % 2 == 0) {
+                    myGui.presentarSalida("       Nº ejecuc par --> Generando secuencia de " + ejecuciones + " num aleatorios: ");
+                    for (int i = 0; i < ejecuciones; i++) {
+                        myGui.presentarSalida((int) (Math.floor(Math.random() * 10) + 1) + ", ");
+                    }
+                } else {
+                    int firstTerm = 0;
+                    int secondTerm = 1;
+                    myGui.presentarSalida("     Nº ejecuc impar --> Generando sec fibonacci de " + ejecuciones + " num: ");
+                    myGui.presentarSalida(firstTerm + ", " + secondTerm + ", ");
+
+                    // Genera los términos de la secuencia de Fibonacci
+                    for (int i = 2; i < ejecuciones; i++) {
+                        int nextTerm = firstTerm + secondTerm;
+                        myGui.presentarSalida(nextTerm + ", ");
+
+                        firstTerm = secondTerm;
+                        secondTerm = nextTerm;
+                    }
+                }
+            }
         }
-        
+
     }
-    
+
 }
---
+++ src/main/java/com/uja/ssmmaa/primeragente/agentes/AgenteMonitor.java
@@ -0,0 +1,170 @@
+package com.uja.ssmmaa.primeragente.agentes;
+
+import com.uja.ssmmaa.primeragente.gui.AgenteMonitorJFrame;
+import jade.core.Agent;
+import jade.core.MicroRuntime;
+import jade.core.behaviours.TickerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author pedroj Esqueleto de agente para la estructura general que deben tener
+ * todos los agentes
+ */
+public class AgenteMonitor extends Agent {
+
+    //Variables del agente
+    private String nombreFichero;
+    private ArrayList<String> arrayNombreAgentes;
+    private ArrayList<String> arrayClaseAgentes;
+    private ArrayList<String> arrayEjecucionesAgentes;
+    private AgenteMonitorJFrame myGui2;
+
+    private String nombreAgente;
+    private String claseAgente;
+    private String numEjecuciones;
+    private int tiempoCreacionAgentes;
+    //    private String textaco;
+    //    private int numEjecuciones;
+
+    @Override
+    protected void setup() {
+        try {
+            //Inicialización de las variables del agente
+            //archivo="configuracion.txt";
+            leerArchivo();
+
+            //Configuración del GUI
+            myGui2 = new AgenteMonitorJFrame(this);
+            myGui2.setVisible(true);
+            myGui2.presentarSalida("Se inicializa la ejecución de " + this.getName() + "\n");
+            System.out.println("Se inicia la ejecución del agente: " + this.getName());
+            //Registro del agente en las Páginas Amarrillas
+
+            //Registro de la Ontología
+            DFAgentDescription dfd = new DFAgentDescription();
+            dfd.setName(getAID());
+            ServiceDescription sd = new ServiceDescription();
+            sd.setType("Tipo de Servicio");
+            sd.setName("Nombre del Servicio");
+            dfd.addServices(sd);
+            try {
+                DFService.register(this, dfd);
+            } catch (FIPAException fe) {
+                fe.printStackTrace();
+            }
+
+            // Se añaden las tareas principales
+            addBehaviour(new TareaCrearAgentes(this, tiempoCreacionAgentes * 1000));
+        } catch (Exception ex) {
+            Logger.getLogger(AgenteMonitor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        //Añadir las tareas principales
+    }
+
+    @Override
+    protected void takeDown() {
+        //Desregistro de las Páginas Amarillas
+        try {
+            DFService.deregister(this);
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+
+        //Se liberan los recuros y se despide
+        myGui2.dispose();
+        System.out.println("Finaliza la ejecución de " + this.getName());
+        MicroRuntime.stopJADE();
+    }
+
+    //Métodos de trabajo del agente
+    private void leerArchivo() throws Exception {
+        Object[] args = getArguments();
+        if (args != null && args.length > 0) {
+            // Lee el fichero de configuración
+            nombreFichero = (String) args[0];
+            System.out.println("****LEYENDO ARCHIVO: " + nombreFichero + " ****");
+            try (BufferedReader reader = new BufferedReader(new FileReader(nombreFichero))) {
+                String linea = reader.readLine();
+                tiempoCreacionAgentes = Integer.parseInt(linea);
+
+                arrayNombreAgentes = new ArrayList<>();
+                arrayClaseAgentes = new ArrayList<>();
+                arrayEjecucionesAgentes = new ArrayList<>();
+
+                while ((linea = reader.readLine()) != null) {
+                    String[] argumentos = linea.split(":");
+                    nombreAgente = argumentos[0];
+                    claseAgente = argumentos[1].split(" ")[0];
+                    numEjecuciones = argumentos[1].split(" ")[1];
+
+                    //                    String[] elementos = linea.split(":");
+                    //                    String nombreAgente = elementos[0];
+                    //                    int ejecuciones = Integer.parseInt(elementos[1]);
+                    arrayNombreAgentes.add(nombreAgente);
+                    System.out.println("Nombre: " + nombreAgente);
+                    arrayClaseAgentes.add(claseAgente);
+                    System.out.println("Clase: " + claseAgente);
+                    arrayEjecucionesAgentes.add(numEjecuciones);
+                    System.out.println("Ejecuciones: " + numEjecuciones);
+
+                }
+            } catch (IOException ex) {
+                System.err.println("Error al leer el fichero de configuración: " + ex.getMessage());
+                throw new Exception();
+            }
+        }
+    }
+
+    //Clases internas que representan las tareas del agente
+    public class TareaCrearAgentes extends TickerBehaviour {
+
+        //Tarea de ejemplo que se repite cada 10 segundos
+        public TareaCrearAgentes(Agent a, long period) {
+            super(a, period);
+        }
+
+        @Override
+        protected void onTick() {
+            
+            String arrayAuxiliar[] = new String[1];
+            if (!arrayNombreAgentes.isEmpty()) {
+                Object[] arrAux=new Object[1];
+                arrAux[0]=arrayEjecucionesAgentes.get(0);
+                System.out.println("Creando agente...");
+                arrayEjecucionesAgentes.remove(0);
+                try {
+                    System.out.println(arrayNombreAgentes.get(0) + "; "+arrayClaseAgentes.get(0)+" "+arrAux[0]);
+                    MicroRuntime.startAgent(arrayNombreAgentes.get(0), arrayClaseAgentes.get(0), arrAux);
+                    arrayNombreAgentes.remove(0);
+                    arrayClaseAgentes.remove(0);
+                } catch (Exception ex) {
+                    Logger.getLogger(AgenteMonitor.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }else{
+                myAgent.doDelete();
+                System.out.println("Finaliza la ejecución de " + this.myAgent.getName() + " quedarse sin agentes por crear");
+            }
+
+        }
+    }
+
+    public int getTiempoCreacionAgentes() {
+        return tiempoCreacionAgentes;
+    }
+
+}
